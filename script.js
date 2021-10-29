fbc82955f8c219afd537ce0868d9d9b08695b34d
Vasile Midrigan <111rigan111@gmail.com>
2021-10-29 23:13:31 +0100

    new README.md file + code refactoring

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 /* Variables */
 const secretNumber = Math.floor(Math.random() * 30 + 1);
+console.log("secretNumber:", secretNumber);
 const buttonCheck = document.querySelector(".button_check");
+let livesCountTextContent = document.querySelector(".lives_count").textContent;
 /* Score count variables*/
 let score = 20;
 document.querySelector(".lives_count").textContent = score;
+/* Display message function */
+const displayMessage = (message) => {
+  document.querySelector(".message").textContent = message;
+};
 /* Checking the guess number */
 buttonCheck.addEventListener("click", function () {
-  if (document.querySelector(".input_number").value > secretNumber) {
-    document.querySelector(".message").textContent = "☝ too high!";
-    document.querySelector(".input_number").value = "";
-    score--;
-    document.querySelector(".lives_count").textContent = score;
-  } else if (document.querySelector(".input_number").value < secretNumber) {
-    document.querySelector(".message").textContent = "☝ too low!";
-    document.querySelector(".input_number").value = "";
-    score--;
-    document.querySelector(".lives_count").textContent = score;
+  if (document.querySelector(".lives_count").textContent > 0) {
+    if (document.querySelector(".input_number").value > secretNumber) {
+      displayMessage("☝ too high!");
+      document.querySelector(".input_number").value = "";
+      score--;
+      document.querySelector(".lives_count").textContent = score;
+    } else if (document.querySelector(".input_number").value < secretNumber) {
+      displayMessage("☝ too low!");
+      document.querySelector(".input_number").value = "";
+      score--;
+      document.querySelector(".lives_count").textContent = score;
+    } else {
+      displayMessage("🎉 Congratulation");
+      document.querySelector(".wrapper").style.backgroundColor = "purple";
+      document.querySelector(".secret_number").textContent = secretNumber;
+      document.querySelector(".input_number").value = "";
+      document.querySelector(".highscore_count").textContent = score;
+    }
   } else {
-    document.querySelector(".message").textContent = "🎉 Congratulation";
-    document.querySelector(".wrapper").style.backgroundColor = "purple";
-    document.querySelector(".secret_number").textContent = secretNumber;
-    document.querySelector(".input_number").value = "";
-    document.querySelector(".highscore_count").textContent = score;
+    displayMessage("😈 Game Over!");
   }
 });
 /* Restart */
@@ -29,7 +39,7 @@ const restart = document.querySelector(".restart_button");
 restart.addEventListener("click", function () {
   document.querySelector(".wrapper").style.backgroundColor = "#630a10";
   document.querySelector(".secret_number").textContent = "?";
-  document.querySelector(".message").textContent = "🤡 Another one?";
+  displayMessage("🤡 Another one?");
   document.querySelector(".input_number").value = "";
   score = 20;
   document.querySelector(".lives_count").textContent = score;
